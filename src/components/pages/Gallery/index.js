e4a1cc8c19436bc085c139700d3b06d66209cf8b
Ervin156 <dmitriylystov@gmail.com>
2019-06-09 21:24:33 +0300

    create gallery and api photo

--- src/components/pages/Gallery/index.js
+++ src/components/pages/Gallery/index.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React,{useEffect} from 'react';
+import List from './views/List'
+import {connect} from  'react-redux'
 import './style.scss';
+import {getPhotos} from './service'
 
 
-function Gallery() {
+function Gallery(props) {
+  const{setPhotos,photos}= props;
 
+  useEffect(()=>{
+    getPhotos()
+    
+    .then(photos =>{
+      setPhotos(photos);
+    })
+  },[]);
 
   return (
     <div className="gallery">
       <h1>Hi TESTINGsdsdsdsds</h1>
+      < List photos = {photos}/>
+
       
     </div> 
   );
 }
+const mapStateToProps = state=>({
+  photos: state.gallery.photos
+});
 
-export default Gallery;
+const mapDispatchToProps = dispatch=>({
+  setPhotos: photos => dispatch({type: 'SET_PHOTOS',photos})
+});
+
+
+export default connect(mapStateToProps,mapDispatchToProps)(Gallery);
--- src/core/RooReducer.js
+++ src/core/RooReducer.js
@@ -2,9 +2,12 @@ import {combineReducers} from 'redux'
 
 
 import app from '../components/App/redux/reducer'
+import gallery from '../components/pages/Gallery/redux/reducer';
+
 
 
 export default combineReducers ({
     app,
+    gallery,
 
 })
